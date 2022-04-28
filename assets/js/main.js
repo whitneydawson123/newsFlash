320a082238a7448697f229c95e1f0b8ef1ec0959
whitneydawson123 <whitneydawson123@gmail.com>
2022-04-28 17:55:38 -0400

    swiper finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,11 @@ const selectElement = selector => {
 };
 //Nav styles on scroll
 const scrollHeader = () => {
-  const headerElement = selectElement('header');
+  const navbarElement = selectElement('header');
   if (this.scrollY >= 15) {
-    headerElement.classList.add('activated');
+    navbarElement.classList.add('activated');
   } else {
-    headerElement.classList.remove('activated');
+    navbarElement.classList.remove('activated');
   }
 };
 
@@ -38,7 +38,7 @@ window.addEventListener('keyup', event => {
   if (event.key === 'Escape') searchFormContainer.classList.remove('activated');
 });
 // Switch theme/add to local storage
-const bodyElement = document.body;
+const body = document.body;
 const themeToggleBtn = selectElement('#theme-toggle-btn');
 const currentTheme = localStorage.getItem('currentTheme');
 
@@ -53,10 +53,29 @@ themeToggleBtn.addEventListener('click', () => {
     bodyElement.classList.toggle('light-theme');
   });
 
-  if (bodyElement.classList.contains('light-theme')) {
+  if (body.classList.contains('light-theme')) {
     localStorage.setItem('currentTheme', 'themeActive');
   } else {
     localStorage.removeItem('currentTheme');
   }
 });
 // Swiper
+const swiper = new swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  breakpoints: {
+    700: {
+      slidesPerView: 2
+    },
+    1200: {
+      slidesPerView: 3
+    }
+  }
+});
